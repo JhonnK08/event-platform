78d7ed7edb4321867a7c6b27b1cb6e124ad42e62
Jhonatan <38366068+JhonnK08@users.noreply.github.com>
2022-06-20 22:39:10 -0300

    refact: removed unused components and added providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,29 @@
-import { useState } from "react";
-import "./App.css";
-import logo from "./logo.svg";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSONS_QUERY = gql`
+    query {
+        lessons {
+            id
+            title
+        }
+    }
+`;
+
+interface Lesson {
+    id: string;
+    title: string;
+}
 
 function App() {
-    const [count, setCount] = useState(0);
+    const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+    console.log(data);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello Ignite Lab!</p>
-                <p>
-                    <button
-                        type="button"
-                        onClick={() => setCount((count) => count + 1)}
-                    >
-                        count is: {count}
-                    </button>
-                </p>
-                <p>
-                    Edit <code>App.tsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    {" | "}
-                    <a
-                        className="App-link"
-                        href="https://vitejs.dev/guide/features.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Vite Docs
-                    </a>
-                </p>
-            </header>
-        </div>
+        <ul>
+            {data?.lessons.map((lesson) => {
+                return <li key={lesson.id}>{lesson.title}</li>;
+            })}
+        </ul>
     );
 }
 
