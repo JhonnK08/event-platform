e0975417b8449bf44d64cbc700efc814b007e489
Jhonatan <38366068+JhonnK08@users.noreply.github.com>
2022-06-24 00:04:15 -0300

    feat(lib): added env variables to apollo lib

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: "https://api-sa-east-1.graphcms.com/v2/cl4ovqb2719lf01z46tgvc1k2/master",
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+    },
     cache: new InMemoryCache(),
 });
